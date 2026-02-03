85b12752eb71c493a8bc80a909b3426fa67aab6d
Smockelito <amandavikberg90@gmail.com>
2026-02-03 08:14:41 +0100

    Showing recurrent transactions in view

---
+++ Models/TransactionOccurrence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektuppgiftBudgetplanerare.Models
+{
+    public class TransactionOccurence
+    {
+        public int SourceId { get; set; }
+        public string Category { get; set; } = "";
+        public decimal Amount { get; set; }
+        public bool IsIncome { get; set; }
+        public DateTime Date { get; set; }
+        public TransactionRecurrence Recurrence { get; set; }
+        public string Description { get; set; } = "";
+    }
+}
